62f3fee66a304af6274ecf8d8d1faf47c5c8db28
samlogan <sam@sjlwebdesign.com>
2019-11-09 16:37:35 +1100

    Updated reducers with new linting rules

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import posts from '../reducers/posts';
+import posts from './posts';
 
 const rootReducer = combineReducers({
-  posts
+  posts,
 });
 
 export default rootReducer;
