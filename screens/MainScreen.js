90ea944309ec64bce3d7a13861477b40077bb713
samlogan <sam@sjlwebdesign.com>
2019-11-09 16:44:15 +1100

    Fixed linting errors in components

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  View,
-  StyleSheet,
-  StatusBar,
-  Text
-} from 'react-native';
+import { View, StyleSheet, StatusBar, Text } from 'react-native';
 import { getPosts } from '../actions/posts';
 
 const styles = StyleSheet.create({
@@ -13,17 +8,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#005B93'
+    backgroundColor: '#005B93',
   },
   textStyle: {
     color: '#FFFFFF',
     marginBottom: 10,
-  }
+  },
 });
 
 const MainScreen = () => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
+  const posts = useSelector(state => state.posts);
 
   useEffect(() => {
     dispatch(getPosts());
@@ -38,7 +33,11 @@ const MainScreen = () => {
       <Text style={styles.textStyle}>Edit screens/MainScreen.js to get started.</Text>
       <Text style={styles.textStyle}>Using Redux Hooks to fetch dummy posts:</Text>
       <View>
-        {items.map((post) => <Text key={post} style={styles.textStyle}>{post}</Text>)}
+        {items.map(post => (
+          <Text key={post} style={styles.textStyle}>
+            {post}
+          </Text>
+        ))}
       </View>
     </View>
   );
